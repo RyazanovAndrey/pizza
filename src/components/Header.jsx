595453ab3bfbdf935be41e0fc1ab698cb47e4bb5
RyazanovAndrej <ryazanov.andrej@gmail.com>
2023-07-12 21:41:19 +0300

    21.41

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Logo from '../../public/img/Logo.png'
 import { BiSearch } from "react-icons/bi";
 import { BiShoppingBag } from "react-icons/bi";
@@ -9,8 +9,15 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-  const {cartTotal, cartSum} = useContext(CartCont);
-  const {search, setSearch} = useContext(ProductCont);
+  const { cartTotal, cartSum } = useContext(CartCont);
+  const { search, setSearch, product } = useContext(ProductCont);
+
+  const [opnSearchList, setOpnSearchList] = useState(false)
+
+  const activePizza = (e) => {
+    setSearch(e.target.textContent)
+    setOpnSearchList(false)
+  }
 
   return (
     <header className='header'>
@@ -20,19 +27,28 @@ const Header = () => {
           <div className='search'>
             <div className='search-wrapper'>
               <BiSearch className='search-icon' />
-              <input className='search-field' type="text" placeholder='Шукати піцу...' value={search} onChange={e => setSearch(e.target.value)} />
+              <input onClick={() => setOpnSearchList(true)} className='search-field' type="text" placeholder='Шукати піцу...' value={search} onChange={e => setSearch(e.target.value)} />
+
+              {search && opnSearchList && <div className='search-drop'>
+                <ul className='drop'>
+                  {product.map(item => (
+                    <li onClick={activePizza} className='drop-list'>{item.title}</li>
+                  ))}
+                </ul>
+              </div>}
             </div>
+
             <BiX onClick={() => setSearch('')} className='search-clear' />
           </div>
 
           <Link to='/cart'>
-          <div className="cart">
-            <div className="cart-left">{cartSum} ₴</div>
-            <div className="cart-right">
-              <BiShoppingBag className='cart-icon' />
-              <h3 className='cart-sum'>{cartTotal}</h3>
+            <div className="cart">
+              <div className="cart-left">{cartSum} ₴</div>
+              <div className="cart-right">
+                <BiShoppingBag className='cart-icon' />
+                <h3 className='cart-sum'>{cartTotal}</h3>
               </div>
-          </div>
+            </div>
           </Link>
         </div>
       </div>
